92c4918b3fa614e99ab08eb39d0769966adc8406
AXMEDJONS <hasanovaslam8@gmail.com>
2023-04-01 20:09:59 +0500

    version-4

--- main.js
+++ main.js
@@ -231,54 +231,27 @@ function Dropp() {
 // Выбираем кнопку
 let btn = doc.querySelector(".mode__button");
 // Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function () {
-    let body = doc.body
-    // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-    let dnevnoy__css = doc.querySelector('.dnevnoy__css')
-    // setTimeout(() => {
-    // }, "100");
-    if (dnevnoy__css.href === 'http://127.0.0.1:5502/assets/css/styles.css') {
-        dnevnoy__css.href = './assets/css/style.css'
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let css = doc.querySelector('.css')
+    if (localStorage.getItem('theme') === 'dark') {
+        localStorage.removeItem('theme')
     }
-    else if (dnevnoy__css.href === 'http://127.0.0.1:5502/assets/css/style.css') {
-        dnevnoy__css.href = './assets/css/styles.css'
+    else {
+        localStorage.setItem('theme', 'dark')
     }
+    replacement__css()
 });
 
-// let drag_and_drop = () => {
-//     let zone = document.querySelectorAll('.swiper-wrapper')
-//     let children;
-//     let prevs;
-//     function dr_st() {
-//         prevs = this
-//         children = this.children
-//     }
-//     function dr_ent() { }
-//     function dr_ov(event) {
-//         event.preventDefault()
-//     }
-//     function dr_lv() { }
-//     function dr_end() { }
-//     function dr_op() {
-//         for (let item of children) {
-//             this.append(item)
-//         }
-//         let child;
-//         child = this.children
-//         for (let item2 of child) {
-//             prevs.append(item2)
-//         }
-//     }
-
-   
-//     for (let item of zone ) {
-//         item.addEventListener('dragstart', dr_st)
-//         item.addEventListener('dragenter', dr_ent)
-//         item.addEventListener('dragover', dr_ov)
-//         item.addEventListener('dragleave', dr_lv)
-//         item.addEventListener('dragend', dr_end)
-//         item.addEventListener('drop', dr_op)
-       
-//     }
-// }
-// drag_and_drop()
+function replacement__css() {
+    let css = doc.querySelector('.css')
+    try {
+        if (localStorage.getItem('theme') === 'dark') {
+            css.href = './assets/css/style.css'
+        }
+        else {
+            css.href = './assets/css/styles.css'
+        }
+    } catch (error) { }
+}
+replacement__css()
